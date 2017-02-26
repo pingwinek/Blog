6e4e75dd0924335963f53b467ca70d0416faf036
Łukasz <devmaterials@gmail.com>
2017-02-26 19:07:12 +0100

    Fixing routes

--- JustBlog/JustBlog.Core/RepositoryModule.cs
+++ JustBlog/JustBlog.Core/RepositoryModule.cs
@@ -6,6 +6,7 @@ using NHibernate.Tool.hbm2ddl;
 using Ninject;
 using Ninject.Modules;
 using Ninject.Web.Common;
+using System.Reflection;
 
 namespace JustBlog.Core
 {
@@ -14,23 +15,18 @@ namespace JustBlog.Core
         public override void Load()
         {
             Bind<ISessionFactory>()
-                .ToMethod
-                (
-                    e =>
-                        Fluently.Configure()
-                        .Database(MsSqlConfiguration.MsSql2008.ConnectionString(c =>
-                            c.FromConnectionStringWithKey("JustBlogDbConnString")))
-                        .Cache(c => c.UseQueryCache().ProviderClass<HashtableCacheProvider>())
-                        .Mappings(m => m.FluentMappings.AddFromAssemblyOf<Post>())
-                        .ExposeConfiguration(cfg => new SchemaExport(cfg).Execute(true, true, false))
-                        .BuildConfiguration()
-                        .BuildSessionFactory()
-                )
-                .InSingletonScope();
+              .ToMethod(e => Fluently.Configure()
+              .Database(MsSqlConfiguration.MsSql2008.ConnectionString(c => c.FromConnectionStringWithKey("JustBlogDbConnString")))
+              .Cache(c => c.UseQueryCache().ProviderClass<HashtableCacheProvider>())
+              .Mappings(m => m.FluentMappings.AddFromAssemblyOf<Post>())
+              //.ExposeConfiguration(cfg => new SchemaExport(cfg).Execute(true, true, false))
+              .BuildConfiguration()
+              .BuildSessionFactory())
+              .InSingletonScope();
 
             Bind<ISession>()
-                .ToMethod((ctx) => ctx.Kernel.Get<ISessionFactory>().OpenSession())
-                .InRequestScope();
+              .ToMethod((ctx) => ctx.Kernel.Get<ISessionFactory>().OpenSession())
+              .InRequestScope();
         }
     }
 }
--- JustBlog/JustBlog/App_Start/RouteConfig.cs
+++ JustBlog/JustBlog/App_Start/RouteConfig.cs
@@ -14,9 +14,9 @@ namespace JustBlog
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                "Default",
+                "{controller}/{action}/{id}",
+                new { controller = "Blog", action = "Posts", id = UrlParameter.Optional }
             );
         }
     }
