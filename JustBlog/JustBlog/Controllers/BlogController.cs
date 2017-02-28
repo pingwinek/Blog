66f43522afbdafeebd7a702b5d113ce21e006fb3
Łukasz <devmaterials@gmail.com>
2017-03-01 00:00:11 +0100

    Create an action to return posts for a particular tag

--- JustBlog/JustBlog/Controllers/BlogController.cs
+++ JustBlog/JustBlog/Controllers/BlogController.cs
@@ -27,13 +27,25 @@ namespace JustBlog.Controllers
 
         public ViewResult Category(string category, int p = 1)
         {
-            var viewModel = new ListViewModel(_blogRepository, category, p);
+            var viewModel = new ListViewModel(_blogRepository, category, "Category", p);
 
             if (viewModel.Category == null)
                 throw new HttpException(404, "Category not found");
 
             ViewBag.Title = String.Format(@"Latest posts on category ""{0}""",
-                                viewModel.Category.Name);
+                viewModel.Category.Name);
+            return View("List", viewModel);
+        }
+
+        public ViewResult Tag(string tag, int p = 1)
+        {
+            var viewModel = new ListViewModel(_blogRepository, tag, "Tag", p);
+
+            if (viewModel.Tag == null)
+                throw new HttpException(404, "Tag not found");
+
+            ViewBag.Title = String.Format(@"Latest posts tagged on ""{0}""",
+                viewModel.Tag.Name);
             return View("List", viewModel);
         }
     }
--- JustBlog/JustBlog/Models/ListViewModel.cs
+++ JustBlog/JustBlog/Models/ListViewModel.cs
@@ -11,6 +11,7 @@ namespace JustBlog.Models
         public IList<Post> Posts { get; private set; }
         public int TotalPosts { get; private set; }
         public Category Category { get; private set; }
+        public Tag Tag { get; private set; }
 
         public ListViewModel(IBlogRepository _blogRepository, int p)
         {
@@ -19,11 +20,21 @@ namespace JustBlog.Models
         }
 
         public ListViewModel(IBlogRepository blogRepository,
-        string categorySlug, int p)
+        string text, string type, int p)
         {
-            Posts = blogRepository.PostsForCategory(categorySlug, p - 1, 10);
-            TotalPosts = blogRepository.TotalPostsForCategory(categorySlug);
-            Category = blogRepository.Category(categorySlug);
+            switch (type)
+            {
+                case "Tag":
+                    Posts = blogRepository.PostsForTag(text, p - 1, 10);
+                    TotalPosts = blogRepository.TotalPostsForTag(text);
+                    Tag = blogRepository.Tag(text);
+                    break;
+                default:
+                    Posts = blogRepository.PostsForCategory(text, p - 1, 10);
+                    TotalPosts = blogRepository.TotalPostsForCategory(text);
+                    Category = blogRepository.Category(text);
+                    break;
+            }
         }
     }
 }
