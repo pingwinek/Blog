ab24cf61b676d4dd4a1ac8da06115ecea6544419
Łukasz <devmaterials@gmail.com>
2017-03-02 00:27:34 +0100

    Update the WidgetViewModel

--- JustBlog/JustBlog/Models/WidgetViewModel.cs
+++ JustBlog/JustBlog/Models/WidgetViewModel.cs
@@ -8,8 +8,10 @@ namespace JustBlog.Models
         public WidgetViewModel(IBlogRepository blogRepository)
         {
             Categories = blogRepository.Categories();
+            Tags = blogRepository.Tags();
         }
 
         public IList<Category> Categories { get; private set; }
+        public IList<Tag> Tags { get; private set; }
     }
 }
