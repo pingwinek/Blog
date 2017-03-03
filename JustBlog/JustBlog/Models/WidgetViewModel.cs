8f221cce1efc9d8e0d75311e7fa3442cbdc42c3f
Łukasz <devmaterials@gmail.com>
2017-03-03 23:36:17 +0100

    Display the latest posts in a widget

--- JustBlog/JustBlog/Models/WidgetViewModel.cs
+++ JustBlog/JustBlog/Models/WidgetViewModel.cs
@@ -9,9 +9,11 @@ namespace JustBlog.Models
         {
             Categories = blogRepository.Categories();
             Tags = blogRepository.Tags();
+            LatestPosts = blogRepository.Posts(0, 10);
         }
 
         public IList<Category> Categories { get; private set; }
         public IList<Tag> Tags { get; private set; }
+        public IList<Post> LatestPosts { get; private set; }
     }
 }
