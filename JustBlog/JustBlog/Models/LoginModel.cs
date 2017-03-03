aa3bce1fc91706ceea94fb714dc9953904788f03
Łukasz <devmaterials@gmail.com>
2017-03-03 23:46:03 +0100

    Create the login view - base

---
+++ JustBlog/JustBlog/Models/LoginModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace JustBlog.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "User name is required")]
+        [Display(Name = "User name (*)")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [Display(Name = "Password (*)")]
+        public string Password { get; set; }
+    }
+}
